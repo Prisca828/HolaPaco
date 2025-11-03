59b25b75ca8cbe82cf6f6fffa67766dfb65b7aa2
Prisca <prigomagu@alu.edu.gva.es>
2025-11-03 10:55:18 +0100

    Subo todo el proyecto

---
+++ src/AdiosRaul.java
@@ -0,0 +1,7 @@
+public class AdiosRaul {
+    public static void main(String[] args) throws Exception {
+        System.out.println("AdiosRaul!");
+
+    }
+}
+
---
+++ src/HolaPaco.java
@@ -0,0 +1,6 @@
+public class HolaPaco {
+      public static void main(String[] args) throws Exception {
+        System.out.println("HolaPaco!");
+
+    }
+}
